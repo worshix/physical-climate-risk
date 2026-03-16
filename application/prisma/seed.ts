e89422373b50f0ffbc2907dc68fb71984e19ebcc
worshix <worshiplivingstone@gmail.com>
2026-03-16 08:46:25 +0200

    added admin seeding

--- application/prisma/seed.ts
+++ application/prisma/seed.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import bcrypt from "bcryptjs";
 
-const dbPath = path.join(process.cwd(), "dev.db");
+const dbPath = `file:${path.join(process.cwd(), "dev.db")}`;
 const adapter = new PrismaBetterSqlite3({ url: dbPath });
 const prisma = new PrismaClient({ adapter });
 
